0b068cc297da519ae3d6b4b1bec9c5e8f081e957
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-09-27 17:30:19 +0900

    json response using marshal

---
+++ README.md
@@ -0,0 +1,4 @@
+### Notes for "Lets Go Further"
+
+## 3 - Sending JSON responses
+- ```func Marshal(v any) ([]byte, error)``` -> pass native Go object and returns JSON representation in []byte
--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,12 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	js = append(js, '\n')
+	w.Header().Set("Content-Type", "application/json")
+	// Use w.Write() to send the []byte slice containing the JSON as the response body.
+	w.Write(js)
 }
